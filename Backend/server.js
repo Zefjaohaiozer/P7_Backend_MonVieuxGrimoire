250552507d935fd7a32c9cf66de3397f09583376
Zefjaohaiozer <apellissier2@gmail.com>
2023-04-22 11:08:55 +0200

    ajout dotenv

--- Backend/server.js
+++ Backend/server.js
@@ -1,49 +1,57 @@
-const http = require('http');
+const https = require('https');
+const fs = require('fs');
 const app = require('./app');
+require('dotenv').config();
 
 const normalizePort = (val) => {
-	const port = parseInt(val, 10);
+  const port = parseInt(val, 10);
 
-	if (isNaN(port)) {
-		return val;
-	}
-	if (port >= 0) {
-		return port;
-	}
-	return false;
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
 };
+
 const port = normalizePort(process.env.PORT || '4000');
 app.set('port', port);
 
 const errorHandler = (error) => {
-	if (error.syscall !== 'listen') {
-		throw error;
-	}
-	const address = server.address();
-	const bind =
-		typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
-	switch (error.code) {
-		case 'EACCES':
-			console.error(bind + ' requires elevated privileges.');
-			process.exit(1);
-			break;
-		case 'EADDRINUSE':
-			console.error(bind + ' is already in use.');
-			process.exit(1);
-			break;
-		default:
-			throw error;
-	}
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind =
+    typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
 };
 
-const server = http.createServer(app);
+const options = {
+  key: fs.readFileSync(process.env.PRIVKEY),
+  cert: fs.readFileSync(process.env.CERT),
+  ca: fs.readFileSync(process.env.FULLCHAIN),
+};
+
+const server = https.createServer(options, app);
 
 server.on('error', errorHandler);
 server.on('listening', () => {
-	const address = server.address();
-	const bind =
-		typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-	console.log('Listening on ' + bind);
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
 });
 
 server.listen(port);
