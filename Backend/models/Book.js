9e94561426aa11bb86e09afbdec0e579404f41df
Zefjaohaiozer <apellissier2@gmail.com>
2023-04-22 11:39:20 +0200

    changements dans le schéma mongoose des livres

--- Backend/models/Book.js
+++ Backend/models/Book.js
@@ -3,20 +3,25 @@ const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
 const ratingSchema = mongoose.Schema({
-	userId: { type: String, required: true, unique: true },
-	grade: { type: Number, required: true },
+  userId: { type: String, required: true },
+  bookId: { type: String, required: true },
+  grade: { type: Number, required: true },
+});
+
+ratingSchema.plugin(uniqueValidator, {
+  message: 'Error, expected {PATH} to be unique.',
 });
 
 const bookSchema = mongoose.Schema({
-	title: { type: String, required: true },
+  title: { type: String, required: true },
 
-	author: { type: String, required: true },
-	year: { type: Number, required: true },
-	genre: { type: String, required: true },
-	userId: { type: String, required: true },
-	imageUrl: { type: String, required: true },
-	ratings: [ratingSchema],
-	averageRating: { type: Number, required: true },
+  author: { type: String, required: true },
+  year: { type: Number, required: true },
+  genre: { type: String, required: true },
+  userId: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  ratings: [ratingSchema],
+  averageRating: { type: Number, required: true },
 });
 
 module.exports = mongoose.model('Book', bookSchema);
